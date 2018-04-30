d3cbca107abe8325d463ce17c7a01de7df2addcd
wangkai <wk_51920@126.com>
2018-04-30 21:09:14 +0800

    init project

---
+++ src/main/java/com/w00429015/App.java
@@ -0,0 +1,13 @@
+package com.w00429015;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
